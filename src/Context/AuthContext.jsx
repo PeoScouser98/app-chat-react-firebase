2cf972b628a4dd38fc7058eddbe3a5c62305f2cf
Quang Hiệp <quanghiep03198@gmail.com>
2022-12-10 22:07:57 +0700

    refactor: context api

---
+++ src/Context/AuthContext.jsx
@@ -0,0 +1,33 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase/firebase.config";
+
+const AuthContext = createContext({});
+
+const AuthProvider = ({ children }) => {
+	const navigate = useNavigate();
+	const [currentUser, setCurrentUser] = useState({});
+
+	useEffect(() => {
+		const unsubcribed = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				const { displayName, email, uid, photoURL } = user;
+				setCurrentUser({ displayName, email, uid, photoURL });
+				navigate("/");
+			} else {
+				setCurrentUser({});
+				navigate("/login");
+			}
+		});
+		return () => {
+			unsubcribed();
+		};
+	}, []);
+	console.log(currentUser);
+
+	return <AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>;
+};
+
+export { AuthContext };
+export default AuthProvider;
---
+++ src/Context/ChatContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useReducer } from "react";
+import { AuthContext } from "./AuthContext";
+
+const ChatContext = createContext({});
+
+const ChatProvider = ({ children }) => {
+	const currentUser = useContext(AuthContext);
+
+	const chatReducer = (state, action) => {
+		switch (action.type) {
+			case "CHANGE_USER":
+				console.log(action.payload);
+				return {
+					user: action.payload.user,
+					chatId:
+						currentUser.uid > action.payload.user.uid
+							? currentUser.uid + "_&_" + action.payload.user.uid
+							: action.payload.user.uid + "_&_" + currentUser.uid,
+				};
+
+			default:
+				return state;
+		}
+	};
+
+	const [chatState, dispatch] = useReducer(chatReducer, {
+		chatId: null,
+		user: null,
+	});
+
+	return <ChatContext.Provider value={{ chatState, dispatch }}>{children}</ChatContext.Provider>;
+};
+
+export { ChatContext };
+export default ChatProvider;
